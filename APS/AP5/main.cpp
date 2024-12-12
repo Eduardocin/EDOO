56466d3682e13e9728910f5cc26bd593c3acb930
Eduardo Henrique da Silva Santana <ehss@WINDOWS.CIN.UFPE.BR>
2024-12-12 11:52:47 -0300

    Ap6

--- APS/AP5/main.cpp
+++ APS/AP5/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-
-#include "fraction.h"
+#include "Fraction.h"
 
 using namespace std;
 
@@ -8,19 +7,6 @@ using namespace std;
 int main(){
     Fraction f1(1, 2);
     Fraction f2(1, 3);
-    Fraction f3;
-    f3 = f1 + f2;
-    cout << f3 << endl;
-    f3 = f1 - f2;
-    cout << f3 << endl;
-    f3 = f1 * f2;
-    cout << f3 << endl;
-    f3 = f1 / f2;
-    cout << f3 << endl;
-    f3 = f1++;
-    cout << f3 << endl;
-    f3 = f1--;
-    cout << f3 << endl;
     return 0;
 
 
